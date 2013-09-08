851df778ef6ee9cf940e8da71e7e398e06fa5194
Fitz181 <edsync256@gmail.com>
2013-09-08 18:15:55 -0400

    updated js, index, and css - still needs a lot of formatting attention

--- assets/js/textChange.js
+++ assets/js/textChange.js
@@ -10,14 +10,16 @@ function textChange(id, item)
 	headers[4] = 'Math';
 	headers[5] = 'Physics';
 	headers[6] = 'Stats';
+    headers[7] = 'Other';
 
 	contents[0] = 'Bio is about trees and stuff';
 	contents[1] = 'Chem has explosions, which is cool';
 	contents[2] = 'CS is the best!';
 	contents[3] = 'Earth, Atmo, and Planetary are about round things';
 	contents[4] = 'Math is hard - and it fundamentally changes you';
-	contents[5] = 'Physics isn\'t that phun';
+	contents[5] = "Physics isn\'t that phun";
 	contents[6] = 'Stats have histograms and junk';
+    contents[7] = 'Other';
 
 	var head;
 	var content;
@@ -27,6 +29,42 @@ function textChange(id, item)
 		case "bio":
 			head = headers[0];
 			content = contents[0];
-			document.getElementById(id).innerHTML=head;
+			document.getElementById(id).innerHTML = head;
+            break;
+        case "chem":
+            head = headers[1];
+            content = contents[1];
+            document.getElementById(id).innerHTML = head;
+            break;
+        case "cs":
+            head = headers[2];
+            content = contents[2];
+            document.getElementById(id).innerHTML = head;
+            break;
+        case "earth":
+            head = headers[3];
+            content = contents[3];
+            document.getElementById(id).innerHTML = head;
+            break;
+        case "math":
+            head = headers[4];
+            content = contents[4];
+            document.getElementById(id).innerHTML = head;
+            break;
+        case "physics":
+            head = headers[5];
+            content = contents[5];
+            document.getElementById(id).innerHTML = head;
+            break;
+        case "stats":
+            head = headers[6];
+            content = contents[6];
+            document.getElementById(id).innerHTML = head;
+            break;
+        default:
+            head = headers[7];
+            content = contents[7];
+            document.getElementById(id).innerHTML = head;
+            break;
 	}
 }
--- index.php
+++ index.php
@@ -60,16 +60,16 @@
 	</div> <!-- end subject_text -->
 
 	<div id='box'>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-		<p>STUFFFFFF</p>
-	</div> <!-- end box -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis metus quis viverra tincidunt. Vestibulum enim arcu, vulputate vel nunc vitae, rutrum mollis enim. Fusce justo est, faucibus nec congue quis, scelerisque quis sapien. Maecenas et posuere arcu. Nullam gravida consectetur rhoncus. Mauris nec turpis eget nulla ullamcorper fermentum. In id vulputate mauris, a aliquet quam. Quisque lobortis dui a sagittis fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus rutrum pellentesque dictum. Cras fermentum massa nec ipsum sollicitudin, ac tempor ante consequat. Integer dolor ipsum, lacinia eget lacinia eu, porttitor in purus. Praesent rutrum malesuada tempor. Nam pharetra mattis nulla, nec luctus metus vestibulum at.</p>
+
+        <p>Vestibulum fringilla libero non ante accumsan condimentum. Duis ipsum libero, faucibus vitae turpis a, sagittis commodo urna. Curabitur imperdiet ante massa, et pellentesque arcu iaculis commodo. Vestibulum a commodo risus. Sed scelerisque turpis sit amet orci dignissim accumsan. Maecenas massa erat, varius at aliquet ut, fermentum quis orci. Aenean egestas massa quis erat tempor, a dictum urna facilisis. Sed turpis leo, aliquet id dignissim vitae, auctor in urna. Fusce fermentum, lacus eget ultrices dictum, augue ligula cursus nunc, ut consequat justo ante a lacus. Integer nibh ipsum, molestie id ipsum nec, ultrices placerat risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed lobortis viverra ligula, et suscipit mauris viverra et.</p>
+
+        <p>Ut nisi massa, consectetur sit amet purus vel, pretium euismod nunc. Curabitur ornare sit amet turpis at tincidunt. Morbi posuere blandit dolor vitae rutrum. Sed elit nibh, elementum eu urna eget, euismod condimentum turpis. Maecenas quis diam sed tortor condimentum placerat at ac libero. In hac habitasse platea dictumst. Praesent dapibus nibh id blandit porttitor. Quisque eget adipiscing ipsum. Integer sed mattis nibh, a rutrum massa. Pellentesque id semper augue. Aliquam eget lorem neque. Pellentesque ac placerat dolor. Phasellus sed venenatis magna, a ornare nunc. Vestibulum tincidunt at augue vitae egestas.</p>
+
+        <p>In lorem nisl, congue nec facilisis et, eleifend sed enim. Morbi ut condimentum mauris, non malesuada sem. Vivamus non pretium dolor. Quisque mattis dui metus, nec euismod elit sodales et. Praesent et diam lacus. Duis et quam ut sem venenatis lacinia. Praesent feugiat, mi vel dignissim scelerisque, ligula nulla adipiscing risus, vitae ornare metus lorem quis nisl. Morbi et convallis mauris, et tempus sem. Quisque hendrerit molestie tellus ut dictum. In aliquam, justo tempor vestibulum convallis, sapien magna faucibus mauris, malesuada eleifend urna ante a orci. Suspendisse mauris quam, mattis eu laoreet convallis, auctor sit amet leo. Morbi nisl sem, pellentesque vel lacus egestas, ornare vehicula lorem. Nunc sapien nisl, tincidunt at aliquam eu, sollicitudin et ante. Duis vestibulum lorem eu nisi pulvinar, eget faucibus nibh sollicitudin. Integer quis luctus diam. Duis vel venenatis purus, quis pulvinar lectus.</p>
+
+        <p>Nullam vitae nisl imperdiet, placerat lorem sit amet, viverra dui. Suspendisse vitae erat tempus urna tempus posuere. Nunc sed odio sit amet elit semper convallis vitae eu lectus. Pellentesque rhoncus, nulla eu iaculis lobortis, odio lorem accumsan turpis, a hendrerit odio enim at enim. Morbi fringilla, orci non suscipit ullamcorper, tellus erat commodo sem, at accumsan nibh lectus quis dui. Sed varius, diam ullamcorper vestibulum feugiat, augue sem feugiat enim, vel varius metus augue a enim. Ut dui arcu, convallis nec vestibulum nec, facilisis ac elit. Curabitur sit amet libero vitae quam varius ullamcorper. Ut condimentum eget mi in laoreet. Mauris pulvinar, urna vestibulum congue pulvinar, erat sapien tincidunt eros, et fermentum justo odio a urna. Vestibulum aliquet elit ut mauris malesuada, in vestibulum metus vestibulum. Quisque tristique congue orci, in faucibus sapien lobortis sed. Vivamus imperdiet condimentum elit, a mollis enim scelerisque at. Phasellus dignissim, orci at adipiscing feugiat, purus libero mollis nisl, vel aliquam mauris lacus a leo. Integer semper malesuada ligula, sit amet pulvinar urna vehicula id.</p>
+    </div> <!-- end box -->
 </div> <!-- end main -->
 
 <div id='footer'>
@@ -80,3 +80,4 @@
 </body>
 
 </html>
+
