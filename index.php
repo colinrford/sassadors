45a2cbe3697214743791d1a92322525badd9254f
Fitz181 <edsync256@gmail.com>
2013-09-10 00:50:39 -0400

    background image + transparency

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 	<title>College of Science Sassadors</title>
 
-	<link href='assets/css/page.css' rel='stylesheet' type='text/css' />
+	<link href='assets/css/style.css' rel='stylesheet' type='text/css' />
 	<link href='assets/imgs/icons/coffee64.png' rel='shortcut icon' type='image/x-icon' />
 	<link href='http://fonts.googleapis.com/css?family=Fauna+One|Rosario:400,400italic,700' rel='stylesheet' type='text/css' />
 
