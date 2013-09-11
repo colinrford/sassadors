026b54725371cb99afe0f3cb3b877f187169eb31
Fitz181 <edsync256@gmail.com>
2013-09-10 20:13:46 -0400

    issues with side bar nav alignment, probably a pixel thing - need to change to em and tune up div arrangement, test.php/css are being used for testing

---
+++ test.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+
+	<title>College of Science Sassadors</title>
+
+	<link href='assets/css/test.css' rel='stylesheet' type='text/css' />
+	<link href='assets/imgs/icons/coffee64.png' rel='shortcut icon' type='image/x-icon' />
+	<link href='http://fonts.googleapis.com/css?family=Fauna+One|Rosario:400,400italic,700' rel='stylesheet' type='text/css' />
+
+    <script src='assets/js/upTop.js'></script>
+    <script src='assets/js/test.js'></script>
+</head>
+
+<body>
+
+<div id='wrapper'>
+
+<div id='header'>
+	<div id='topbar'>
+	</div> <!-- end topbar -->
+
+	<div id='global_nav'>
+		<a href='/'><img src='assets/imgs/icons/logo.png' /></a>
+
+		<div id='nav_bar'>
+		<ul>
+			<li><a href='/'>Ambassadors</a></li>
+			<li><a href='/'>Calendar</a></li>
+			<li><a href='/'>Contact</a></li>
+			<li><a href='/'>Ed is Great</a></li>
+			<li><a href='/'>About</a></li>
+		</ul>
+		</div> <!-- end nav_bar -->
+
+		<div id='social_menu'>
+		</div> <!-- end social_menu -->
+	</div> <!-- end global_nav -->
+
+</div> <!-- end header -->
+
+<div id='side'>
+    <div id='side_icons'>
+        <span class='icon' id='icon_all'></span>
+        <span class='icon' id='icon_bio'></span>
+        <span class='icon' id='icon_chem'></span>
+        <span class='icon' id='icon_cs'></span>
+        <span class='icon' id='icon_earth'></span>
+        <span class='icon' id='icon_math'></span>
+        <span class='icon' id='icon_phys'></span>
+        <span class='icon' id='icon_stats'></span>
+    </div>
+
+	<div id='side_bar'>
+        <span class='subjects' id='all'> All</span>
+		<span class='subjects' id='bio'>Biological Sciences</span>
+		<span class='subjects' id='chem'>Chemistry</span>
+		<span class='subjects' id='cs'>Computer Science</span>
+		<span class='subjects' id='earth'>Earth, Atmospheric, and Planetary</span>
+		<span class='subjects' id='math'>Mathematics</span>
+		<span class='subjects' id='phys'>Physics</span>
+		<span class='subjects' id='stat'>Statistics</span>
+	</div> <!-- end side_bar -->
+
+    <div id='subject_text'>
+        NOT WORKING!
+        <!--
+        <h4>Header</h4>
+        <hr />
+        <p>This is the default text that will change on a click of a particular nav bar.</p>
+        -->
+    </div> <!-- end subject_text -->
+    <script src='assets/js/test()'></script>
+</div> <!-- end side -->
+
+<div id='main'>
+	<div id='box'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis metus quis viverra tincidunt. Vestibulum enim arcu, vulputate vel nunc vitae, rutrum mollis enim. Fusce justo est, faucibus nec congue quis, scelerisque quis sapien. Maecenas et posuere arcu. Nullam gravida consectetur rhoncus. Mauris nec turpis eget nulla ullamcorper fermentum. In id vulputate mauris, a aliquet quam. Quisque lobortis dui a sagittis fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus rutrum pellentesque dictum. Cras fermentum massa nec ipsum sollicitudin, ac tempor ante consequat. Integer dolor ipsum, lacinia eget lacinia eu, porttitor in purus. Praesent rutrum malesuada tempor. Nam pharetra mattis nulla, nec luctus metus vestibulum at.</p>
+
+        <p>Vestibulum fringilla libero non ante accumsan condimentum. Duis ipsum libero, faucibus vitae turpis a, sagittis commodo urna. Curabitur imperdiet ante massa, et pellentesque arcu iaculis commodo. Vestibulum a commodo risus. Sed scelerisque turpis sit amet orci dignissim accumsan. Maecenas massa erat, varius at aliquet ut, fermentum quis orci. Aenean egestas massa quis erat tempor, a dictum urna facilisis. Sed turpis leo, aliquet id dignissim vitae, auctor in urna. Fusce fermentum, lacus eget ultrices dictum, augue ligula cursus nunc, ut consequat justo ante a lacus. Integer nibh ipsum, molestie id ipsum nec, ultrices placerat risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed lobortis viverra ligula, et suscipit mauris viverra et.</p>
+
+        <p>Ut nisi massa, consectetur sit amet purus vel, pretium euismod nunc. Curabitur ornare sit amet turpis at tincidunt. Morbi posuere blandit dolor vitae rutrum. Sed elit nibh, elementum eu urna eget, euismod condimentum turpis. Maecenas quis diam sed tortor condimentum placerat at ac libero. In hac habitasse platea dictumst. Praesent dapibus nibh id blandit porttitor. Quisque eget adipiscing ipsum. Integer sed mattis nibh, a rutrum massa. Pellentesque id semper augue. Aliquam eget lorem neque. Pellentesque ac placerat dolor. Phasellus sed venenatis magna, a ornare nunc. Vestibulum tincidunt at augue vitae egestas.</p>
+
+        <p>In lorem nisl, congue nec facilisis et, eleifend sed enim. Morbi ut condimentum mauris, non malesuada sem. Vivamus non pretium dolor. Quisque mattis dui metus, nec euismod elit sodales et. Praesent et diam lacus. Duis et quam ut sem venenatis lacinia. Praesent feugiat, mi vel dignissim scelerisque, ligula nulla adipiscing risus, vitae ornare metus lorem quis nisl. Morbi et convallis mauris, et tempus sem. Quisque hendrerit molestie tellus ut dictum. In aliquam, justo tempor vestibulum convallis, sapien magna faucibus mauris, malesuada eleifend urna ante a orci. Suspendisse mauris quam, mattis eu laoreet convallis, auctor sit amet leo. Morbi nisl sem, pellentesque vel lacus egestas, ornare vehicula lorem. Nunc sapien nisl, tincidunt at aliquam eu, sollicitudin et ante. Duis vestibulum lorem eu nisi pulvinar, eget faucibus nibh sollicitudin. Integer quis luctus diam. Duis vel venenatis purus, quis pulvinar lectus.</p>
+
+        <p>Nullam vitae nisl imperdiet, placerat lorem sit amet, viverra dui. Suspendisse vitae erat tempus urna tempus posuere. Nunc sed odio sit amet elit semper convallis vitae eu lectus. Pellentesque rhoncus, nulla eu iaculis lobortis, odio lorem accumsan turpis, a hendrerit odio enim at enim. Morbi fringilla, orci non suscipit ullamcorper, tellus erat commodo sem, at accumsan nibh lectus quis dui. Sed varius, diam ullamcorper vestibulum feugiat, augue sem feugiat enim, vel varius metus augue a enim. Ut dui arcu, convallis nec vestibulum nec, facilisis ac elit. Curabitur sit amet libero vitae quam varius ullamcorper. Ut condimentum eget mi in laoreet. Mauris pulvinar, urna vestibulum congue pulvinar, erat sapien tincidunt eros, et fermentum justo odio a urna. Vestibulum aliquet elit ut mauris malesuada, in vestibulum metus vestibulum. Quisque tristique congue orci, in faucibus sapien lobortis sed. Vivamus imperdiet condimentum elit, a mollis enim scelerisque at. Phasellus dignissim, orci at adipiscing feugiat, purus libero mollis nisl, vel aliquam mauris lacus a leo. Integer semper malesuada ligula, sit amet pulvinar urna vehicula id.</p>
+    </div> <!-- end box -->
+</div> <!-- end main -->
+
+<div id='footer'>
+</div> <!-- end footer -->
+
+</div> <!-- end wrapper -->
+
+</body>
+
+</html>
+
